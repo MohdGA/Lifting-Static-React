714d30ef0d1c53a9bd123826e654886c9ffe532f
MohdGA <generalassemblyse@gmail.com>
2025-08-01 17:19:49 +0300

    lesson finished

---
+++ lectures/react-components/src/components/FounderListItem/FounderListItem.jsx
@@ -0,0 +1,11 @@
+const FounderListItem = (props) => {
+  return (
+    <li>
+      <h3>Hello, I'm {props.founder.name}, the {props.founder.title}!</h3>
+      <p>{props.founder.credential}</p>
+
+    </li>
+  );
+};
+
+export default FounderListItem;
