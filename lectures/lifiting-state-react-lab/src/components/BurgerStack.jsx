9d467d24ee78f1955f2aa5cdf5fcdd197308e688
MohdGA <generalassemblyse@gmail.com>
2025-08-02 22:13:55 +0300

    finished lab + lvl up q2

--- lectures/lifiting-state-react-lab/src/components/BurgerStack.jsx
+++ lectures/lifiting-state-react-lab/src/components/BurgerStack.jsx
@@ -2,13 +2,19 @@ const BurgerStack = (props) => {
   return (
     <>
         <ul>
-            {props.stack.map((food, idx) => 
+            {props.stack.length > 0  ? 
+            
+            props.stack.map((food, idx) => 
                 <li key={idx} style={{ backgroundColor: food.color }}>
                 {food.name}
 
                 <button onClick={() => props.removeToBurger(food)}>X</button>
               </li>
-            )}
+            )
+          
+            : <strong>No Ingredients </strong> 
+          }
+            
         </ul>
     </>
   );
